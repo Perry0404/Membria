6912cdd50bc605c9d2dfde115c9307c419de2e73
Perry0404 <perrypaschal0404@gmail.com>
2025-12-01 15:58:47 +0100

    feat: Add initial project structure and basic files

---
+++ contracts/src/interfaces/IHotState.sol
@@ -0,0 +1,7 @@
+﻿// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IHotState {
+    function read(bytes32 key) external view returns (bytes32);
+    function write(bytes32 key, bytes32 value) external;
+}
---
+++ setup-files.ps1
@@ -0,0 +1,2 @@
+﻿# This script will be used to verify all files are created
+Write-Host "Creating remaining project files..." -ForegroundColor Green
